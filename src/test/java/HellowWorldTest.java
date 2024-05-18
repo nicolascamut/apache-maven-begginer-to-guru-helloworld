cd380fec41fdcfddf6c46509a8ce663160dc45f9
Nicolas Camut <nicolas.camut@paf.com>
2024-05-18 16:51:13 +0300

    Common Maven Plugins Section: clean, compiler, resources, surefire, jar, deploy and site

---
+++ src/test/java/HellowWorldTest.java
@@ -0,0 +1,11 @@
+public class HellowWorldTest {
+
+    public void testHello() {
+        System.out.println("TEST - Hello...");
+    }
+
+    public void testworld() {
+        System.out.println("TEST - World!!!");
+    }
+
+}
